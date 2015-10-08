768cc7b2a4d43594a77e2fc0af3f8f00de78caca
Yuki Kurata <yuki.kurata@gmail.com>
2015-10-08 15:59:41 +0900

    example

--- example/section1.js
+++ example/section1.js
@@ -1,5 +1,6 @@
-
-var sequantial = require('..');
+/*global Cocotte*/
+var isClient = typeof window === 'object';
+var sequantial = isClient ? Cocotte.sequantial : require('..');
 
 var target = {
   list: ['a', 'b', 'c']
